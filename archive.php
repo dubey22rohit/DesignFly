4b4b2e327793c91e6554ecd79d6ca87695e91b5a
Rohit Dubey <dubey22rohit@gmail.com>
2020-10-08 10:45:14 +0530

    Styled Archive Pages

--- archive.php
+++ archive.php
@@ -8,43 +8,51 @@
  */
 
 get_header();
+get_template_part( 'template-parts/content', 'portfolio-tax' );
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+<div id="primary" class="content-area show-sidebar">
+		<main id="main" class="site-main">
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			if ( have_posts() ) :
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				?>
+				<div class="blog-content">
+					<div class="post-content">
+						<div class="main-title">
+							<span class="title"><?php the_archive_title(); ?></span>
+						</div>
+						<div class="post-grid">
+							<?php
+							while ( have_posts() ) {
+								the_post();
+								get_template_part( 'template-parts/content', 'archive' );
+							};
+							?>
+						</div>
+					</div>
+					<?php get_sidebar(); ?>
+				</div>
+				<?php
 
-			endwhile;
+				the_posts_pagination(
+					array(
+						'screen_reader_text' => ' ',
+						'next_text'          => '<img src="' . get_template_directory_uri() . '/img/caret-right-solid.svg">',
+						'prev_text'          => '<img src="' . get_template_directory_uri() . '/img/caret-left-solid.svg">',
+					)
+				);
 
-			the_posts_navigation();
+			else :
 
-		else :
+				get_template_part( 'template-parts/content', 'none' );
 
-			get_template_part( 'template-parts/content', 'none' );
+			endif;
+			?>
 
-		endif;
-		?>
-
-	</main><!-- #main -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 get_sidebar();
--- page.php
+++ page.php
@@ -26,17 +26,15 @@ get_template_part( 'template-parts/content', 'portfolio-tax' );
 				<div class="blog-content">
 					<div class="single-post-content">
 						<?php
-						while ( have_posts() ) :
+						while ( have_posts() ){
 							the_post();
 
 							get_template_part( 'template-parts/content', '' );
-
-							// If comments are open or we have at least one comment, load up the comment template.
 							if ( comments_open() || get_comments_number() ) :
 								comments_template();
 							endif;
 
-						endwhile; // End of the loop.
+						} 
 						?>
 					</div>
 					<?php get_sidebar(); ?>
