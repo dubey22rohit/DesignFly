a5b3a563b1d9727c99c06602096b587ea2b41867
Rohit Dubey <dubey22rohit@gmail.com>
2020-10-08 12:16:36 +0530

    Fixed Single Blog Page

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,53 +11,18 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				designfly_posted_on();
-				designfly_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<?php the_title( '<span class="entry-title">', '</span>' ); ?>
+		<div class="entry-header-meta">
+			<span><?php designfly_posted_by(); ?>&nbsp;<?php designfly_posted_on(); ?></span>
+			<span class="number-of-comments"><?php echo esc_html( get_comments_number() ) . ' ' . esc_html__( 'comments', 'designfly' ); ?></span>
+		</div>
 	</header><!-- .entry-header -->
 
-	<?php designfly_post_thumbnail(); ?>
-
 	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'designfly' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'designfly' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
+		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
+	<div class="entry-footer">
 		<?php designfly_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	</div><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
