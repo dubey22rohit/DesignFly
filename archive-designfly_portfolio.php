6f52012abe5f6cfd2977e7343b46de596073d500
Rohit Dubey <dubey22rohit@gmail.com>
2020-10-08 14:13:31 +0530

    Added Portfolio Archive Page

---
+++ archive-designfly_portfolio.php
@@ -0,0 +1,71 @@
+<?php
+get_header();
+get_template_part( 'template-parts/content', 'portfolio-tax' );
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<div class="main-title">
+				<span><?php echo esc_html__( "D'SIGN IS THE SOUL", 'designfly' ); ?></span>
+				<div class="main-title-button">
+					<?php
+					$terms = get_terms(
+						array(
+							'taxonomy'   => 'designfly_categories',
+							'hide_empty' => false,
+							'number'     => 3,
+						)
+					);
+
+					foreach ( $terms as $t ) {
+						?>
+						<a class="view-all-button" href="<?php echo esc_url( get_term_link( $t, 'designfly_portfolio' ) ); ?>"><?php echo esc_html( $t->name ); ?></a>
+						<?php
+					}
+					?>
+				</div>
+			</div>
+
+			<div class="portfolio-block">
+				<?php
+				if ( have_posts() ) :
+
+					?>
+					<div class="portfolio-grid">
+					<?php
+
+					while ( have_posts() ) :
+
+						the_post();
+
+						get_template_part( 'template-parts/content', 'portfolio' );
+
+						endwhile;
+
+					?>
+					</div>
+					<?php
+
+					the_posts_pagination(
+						array(
+							'screen_reader_text' => ' ',
+							'next_text'          => '<img src="' . get_template_directory_uri() . '/img/caret-right-solid.svg">',
+							'prev_text'          => '<img src="' . get_template_directory_uri() . '/img/caret-left-solid.svg">',
+						)
+					);
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
+				?>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_template_part( 'template-parts/content', 'portfolio-modal' );
+get_footer();
