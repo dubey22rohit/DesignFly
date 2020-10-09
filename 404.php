89fc5c6a1706855c024f1c2ca75304943f45240b
Rohit Dubey <dubey22rohit@gmail.com>
2020-10-09 22:24:48 +0530

    Added 404 file

--- 404.php
+++ 404.php
@@ -10,51 +10,51 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'designfly' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'designfly' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'designfly' ); ?></h2>
-						<ul>
+<div id="primary" class="content-area show-sidebar">
+		<main id="main" class="site-main">
+			<div class="blog-content">
+				<div class="post-content">
+					<div class="main-title">
+						<span class="title">
+							<?php esc_html_e( 'Oops! That page can\'t be found.', 'designfly' ); ?>
+						</span>
+					</div>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'designfly' ); ?></p>
+					<div class="post-grid">
 							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
+							get_search_form();
 							?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$designfly_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'designfly' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$designfly_archive_content" );
+							<div class="widget widget_categories">
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'designfly' ); ?></h2>
+								<ul>
+									<?php
+									wp_list_categories(
+										array(
+											'orderby'    => 'count',
+											'order'      => 'DESC',
+											'show_count' => 1,
+											'title_li'   => '',
+											'number'     => 10,
+										)
+									);
+									?>
+								</ul>
+							</div>
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+							<?php
+							
+							$designfly_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'designfly' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$designfly_archive_content" );
+							the_widget( 'WP_Widget_Tag_Cloud' );
+							?>
+					</div>
+				</div>
+				<?php get_sidebar(); ?>
+			</div>
 
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+		</main>
+	</div>
 
 <?php
 get_footer();
